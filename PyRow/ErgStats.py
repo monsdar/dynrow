5c17b84b3a8c3c910d9fee5fe61973c31d807bff
Nils Brinkmann <monsdar+github@gmail.com>
2015-02-22 12:11:12 +0100

    Changed: Minor formatting and smaller changes

--- PyRow/ErgStats.py
+++ PyRow/ErgStats.py
@@ -4,36 +4,37 @@ import time
 
 try:
     from PyRow import pyrow
-except Exception as e:
-    print "Error importing Pyrow: " + str(e)
+except ImportError:
+    print "Error importing Pyrow"
 
 
-class ErgStats:
+class ErgStats(object):
     erg = None
-    distance = 0.0 #distance in m
-    spm = 20       #Strokes per Minute
-    pace = 135.2   #pace in seconds (2:15.0 equals 135.0)
-    avgPace = 0.0  #the average pace for the current session
-    calhr = 655.0  #These are the approx. calories per hour the user burns
-    power = 150    #Power in Watts
-    calories = 0   #Calories burned away
-    heartrate = 155#Heartrate
-    time = 0.0     #the time of the ergometer, this is important to use because it pauses if the user pauses etc
-    prevTime = 0.0 #needed to see if the time has been updated (else the player is pausing or something similar)
+    distance = 0.0   # distance in m
+    spm = 20         # Strokes per Minute
+    pace = 135.2     # pace in seconds (2:15.0 equals 135.0)
+    avgPace = 0.0    # the average pace for the current session
+    calhr = 0.0      # These are the approx. calories per hour the user burns
+    power = 150      # Power in Watts
+    calories = 0     # Calories burned away
+    heartrate = 155  # Heartrate
+    time = 0.0       # the time of the ergometer, this is important to use because it pauses if the user pauses etc
 
-    numQueries = 0 #the number of queries done to the ergometer. This is needed e.g. to calc the average pace
+    # internal stuff (how to hide?)
+    prevTime = 0.0   # needed to see if the time has been updated (else the player is pausing or something similar)
+    numQueries = 0   # the number of queries done to the ergometer. This is needed e.g. to calc the average pace
 
     @staticmethod
     def initialize():
         try:
             # connect to the ergometer, this call could block some time...
             ergs = []
-            while (len(ergs) == 0):
+            while len(ergs) == 0:
                 ergs = pyrow.find()
             ErgStats.erg = pyrow.pyrow(ergs[0])
             print "Connected to erg"
 
-            #Loop until workout has begun
+            # Loop until workout has begun
             print "Waiting for workout to start ..."
             workout = ErgStats.erg.getWorkout()
             while workout['state'] == 0:
@@ -43,15 +44,14 @@ class ErgStats:
                 except:
                     continue
             print "Workout has begun"
-        except Exception as e:
-            print "Error initing the Concept2: " + str(e)
-            return
+        except NameError:
+            print "Error initing the Concept2, PyRow not available"
 
     @staticmethod
     def update():
-        #Get the distance from the Concept2 ergo. Do nothing if errors occur
+        # Get the distance from the Concept2 ergo. Do nothing if errors occur
         try:
-            monitor = ErgStats.erg.getMonitor() #get monitor data for start of stroke
+            monitor = ErgStats.erg.getMonitor()  # get monitor data for start of stroke
             ErgStats.distance = monitor['distance']
             ErgStats.spm = monitor['spm']
             ErgStats.pace = monitor['pace']
@@ -61,29 +61,28 @@ class ErgStats:
             ErgStats.heartrate = monitor['heartrate']
             ErgStats.time = monitor['time']
 
-        except:
-            #print "Error receiving monitor status"
+        except AttributeError:
+            # print "Error receiving monitor status"
             pass
 
-        #calc the average pace
-        #init the value at the first time we're in here
-        if(ErgStats.avgPace <= 0.000001):
+        # calc the average pace
+        # init the value at the first time we're in here
+        if ErgStats.avgPace <= 0.000001:
             ErgStats.avgPace = ErgStats.pace
 
-        #just update the average if the time has changed
-        if(ErgStats.time - ErgStats.prevTime > 0.01): #check if some time has passed
+        # just update the average if the time has changed
+        if ErgStats.time - ErgStats.prevTime > 0.01:  # check if some time has passed
             ErgStats.avgPace = ((ErgStats.avgPace * ErgStats.numQueries) + ErgStats.pace) / (ErgStats.numQueries + 1)
             ErgStats.numQueries += 1
-	
-        #set the prevTime to be able to compare it next cycle
+
+        # set the prevTime to be able to compare it next cycle
         ErgStats.prevTime = ErgStats.time
 
-        #TODO: This is just for testing purposes. It simulates a moving boat by increasing the distance every cycle
-        if (ErgStats.erg == None):
-            #assuming stable 62.5 FPS
-            #NOTE: The following plays the simulation at 10x speed
-            ErgStats.distance += (0.0591715976331361) * 10.0
-            ErgStats.time += 0.016 * 10.0
-            #ErgStats.distance += (0.0591715976331361)
-            #ErgStats.time += 0.016
+        # TODO: This is just for testing purposes. It simulates a moving boat by increasing the distance every cycle
+        if ErgStats.erg is None:
+            # assuming stable 62.5 FPS
+            # NOTE: The following plays the simulation at higher speed for testing purposes
+            speed = 10.0
+            ErgStats.distance += 0.0591715976331361 * speed
+            ErgStats.time += 0.016 * speed
             return
